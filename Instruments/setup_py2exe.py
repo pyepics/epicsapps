829912d8553dc20142887f50d4acce13fa52d0af
Matt Newville <newville@cars.uchicago.edu>
2011-09-14 09:20:02 -0500

    added setup_py2app.py for Mac app

---
+++ Instruments/setup_py2exe.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import py2exe
+
+setup(name="Python Instruments", 
+      windows=[{'script': 'pyepics_instruments', 'icon_resources': [(0, 'instrument.ico')]}],
+      options = dict(py2exe=dict(optimize=0,
+                                 includes=['epics', 'ctypes', 'wx', 'sqlalchemy'],
+                                 )
+                     )
+      )
+
