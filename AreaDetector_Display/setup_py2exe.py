c87fec4ee52875621113157929a9ece8cda86671
Matt Newville <newville@cars.uchicago.edu>
2012-03-06 14:48:57 -0600

    AreaDetector with py2exe

--- AreaDetector_Display/setup_py2exe.py
+++ AreaDetector_Display/setup_py2exe.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 import py2exe
 import epics
+import matplotlib
+matplotlib.use('WXAgg')
 
-setup(name="Epics AD Display",
-      windows=[{'script': 'EpicsAreaDetectorViewer.py',
+setup(name="Epics AreaDetector Display",
+      data_files=matplotlib.get_py2exe_datafiles(),
+      windows=[{'script': 'pyepics_ad_display.py',
                 "icon_resources": [(0, "camera.ico")]}],
       options = dict(py2exe=dict(optimize=1, bundle_files=1, 
                                  includes=['epics', 'ctypes', 'wx', 'numpy', 'Image'],
