2cc40cf9e67adb5275ed49c45e1ad92264b3adab
Matt Newville <newville@cars.uchicago.edu>
2013-03-11 15:22:09 -0500

    added known cameras for GSE 13IDE

--- AreaDetector_Display/pyepics_ad_display.py
+++ AreaDetector_Display/pyepics_ad_display.py
@@ -11,12 +11,17 @@ if (len(sys.argv) > 1 and sys.argv[1].startswith('-d')):
 else:
     from epicsapps.ad_display import AD_Display
 
+cameras = {'Sample Microscope': '13IDEPS1',
+           'IDB Viewscreen': '13IDBPS1',
+           'IDA Viewscreen': '13IDAPS1'}
+
 if __name__ == '__main__':
     prefix = None
     if len(sys.argv) > 1:
         prefix = sys.argv[1]
     
     app = wx.App()
-    frame = AD_Display(prefix=prefix, app=app)
+    frame = AD_Display(prefix=prefix, app=app,
+                       known_cameras=cameras)
     frame.Show()
     app.MainLoop()
