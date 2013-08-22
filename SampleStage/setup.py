c12a5bf4647a67a0f5c60e729654fecc2094605a
Matt Newville <newville@cars.uchicago.edu>
2013-08-22 16:12:46 -0500

    reorganizing epicsapp samplestage... in progress

---
+++ SampleStage/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, setup_keywords
+
+deps = ('wx', 'epics', 'sqlalchemy')
+setup(name = 'epicsapp_instruments',
+      version = '0.2',
+      author = 'Matthew Newville',
+      author_email = 'newville@cars.uchicago.edu',
+      license = 'BSD',
+      description = 'Epics Instruments configuration and management',
+      package_dir = {'epicsapps.instruments': 'lib', 'epicsapps': 'base'},
+      packages = ['epicsapps', 'epicsapps.instruments'],
+      data_files  = [('bin', ['pyepics_instruments.py'])])
+
+
+errmsg = 'WARNING: pyepics_instruments requires Python module "%s"'
+
+for mod in deps:
+    try:
+        a = __import__(mod)
+    except ImportError:
+        print errmsg % mod
+
