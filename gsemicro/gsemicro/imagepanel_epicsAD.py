c3d47c1cd201509d551b2f0758de39de14090187
Matt Newville <newville@cars.uchicago.edu>
2015-06-02 15:36:24 -0500

    imagepanel for epicsAD (definitely a stub)

--- gsemicro/gsemicro/imagepanel_epicsAD.py
+++ gsemicro/gsemicro/imagepanel_epicsAD.py
@@ -16,3 +16,19 @@
                 except:
                     pass
                 time.sleep(0.05)
+
+    @EpicsFunction
+    def Start(self):
+        if not self.cam_adpref.endswith(':'):
+            self.cam_adpref = "%s:" % self.cam_adpref
+        cname = "%s%s1:"% (self.cam_adpref, self.cam_adform.upper())
+        caput("%sEnableCallbacks" % cname, 1)
+        thisdir = os.path.abspath(os.getcwd())
+        thisdir = thisdir.replace('\\', '/').replace('T:/', '/Volumes/Data/')
+
+        caput("%sFilePath" % cname, thisdir)
+        caput("%sAutoSave" % cname, 0)
+        caput("%sAutoIncrement" % cname, 0)
+        caput("%sFileTemplate" % cname, "%s%s")
+        if self.cam_adform.upper() == 'JPEG':
+            caput("%sJPEGQuality" % cname, 90)
