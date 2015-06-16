8753e01e8751c6b5e22f756b6885078f1207dd43
Matt Newville <newville@cars.uchicago.edu>
2015-06-16 16:36:19 -0500

    update imagepanel_*.py to use imagepanel_base

--- sampleviewer/lib/imagepanel_epicsAD.py
+++ sampleviewer/lib/imagepanel_epicsAD.py
@@ -4,19 +4,16 @@
 import wx
 import time
 import os
-import shutil
-import math
 import numpy as np
 from threading import Thread
-from cStringIO import StringIO
-import base64
 
 from epics import PV, Device, caput
 from epics.wx import EpicsFunction
 
 import Image
+from .imagepanel_base import ImagePanel_Base
 
-class ImagePanel_EpicsAD(wx.Panel):
+class ImagePanel_EpicsAD(ImagePanel_Base):
     img_attrs = ('ArrayData', 'UniqueId_RBV', 'NDimensions_RBV',
                  'ArraySize0_RBV', 'ArraySize1_RBV', 'ArraySize2_RBV',
                  'ColorMode_RBV')
@@ -45,6 +42,7 @@ class ImagePanel_EpicsAD(wx.Panel):
         self.ad_cam = Device(prefix + ':cam1:', delim='',
                                    attrs=self.cam_attrs)
 
+        self.writer = writer
         self.config_filesaver(prefix, format)
 
         width = self.ad_cam.ArraySizeX_RBV
@@ -52,37 +50,11 @@ class ImagePanel_EpicsAD(wx.Panel):
        
         self.img_w = float(width+0.5)
         self.img_h = float(height+0.5)
-        self.writer = writer
-        self.cam_name = '-'
+        self.cam_name = prefix
 
         self.imgcount = 0
         self.imgcount_start = 0
         self.last_update = 0.0
-
-        self.scale = 0.60
-        self.count = 0
-        self.last_size = 0
-
-        self.scalebar = None
-        self.circle  = None
-        self.SetBackgroundColour("#EEEEEE")
-        self.starttime = time.clock()
-        self.SetBackgroundStyle(wx.BG_STYLE_PAINT)
-
-        self.Bind(wx.EVT_SIZE, self.onSize)
-        self.Bind(wx.EVT_PAINT, self.onPaint)
-        self.Bind(wx.EVT_LEFT_DOWN, self.onLeftDown)
-
-        self.autosave = True
-        self.last_autosave = 0
-        self.autosave_tmpf = None
-        self.autosave_file = None
-        if autosave_file is not None:
-            path, tmp = os.path.split(autosave_file)
-            self.autosave_file = autosave_file
-            self.autosave_tmpf = os.path.join(path, '_tmp_.jpg')
-        self.autosave_thread = Thread(target=self.onAutosave)
-        self.autosave_thread.daemon = True
 
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.onTimer, self.timer)
@@ -103,112 +75,18 @@ class ImagePanel_EpicsAD(wx.Panel):
         if format.upper() == 'JPEG1:':
             caput("%sJPEGQuality" % cname, 90)
 
-    def onSize(self, evt):
-        frame_w, frame_h = self.last_size = evt.GetSize()
-        self.scale = min(frame_w/self.img_w, frame_h/self.img_h)
-        self.Refresh()
-        evt.Skip()
-
-    def onTimer(self, evt=None):
-        self.Refresh()
-
-    def onLeftDown(self, evt=None):
-        print 'Left Down Event: ', evt.GetX(), evt.GetY()
-        print 'Left Down Panel Size:  ', self.panel_size
-        print 'Left Down Last Size:   ', self.last_size
-        print 'Left Down Bitmap Size: ', self.bitmap_size
-        print 'Left Down Image Scale: ', self.scale
-
-    def onPaint(self, event):
-        self.count += 1
-        now = time.clock()
-        elapsed = now - self.starttime
-        if elapsed >= 2.0 and self.writer is not None:
-            self.writer(" %.2f fps\n" % (self.count/elapsed))
-            self.starttime = now
-            self.count = 0
-
-        self.scale = max(self.scale, 0.05)
-
-        img =  self.GrabWxImage(scale=self.scale, rgb=True)
-        if img is None:
-            return
-        self.image = img
-        bitmap = wx.BitmapFromImage(self.image)
-        img_w, img_h = self.bitmap_size = bitmap.GetSize()
-        pan_w, pan_h = self.panel_size = self.GetSize()
-        pad_w, pad_h = (pan_w-img_w)/2.0, (pan_h-img_h)/2.0
-
-        dc = wx.AutoBufferedPaintDC(self)
-        dc.Clear()
-        dc.DrawBitmap(bitmap, pad_w, pad_h, useMask=True)
-        dc.BeginDrawing()
-        if self.scalebar is not None:
-            x0, x1, y0, y1, color, width = self.scalebar
-            # x0, y0, x1, y1 = img_w-20, img_h-60, img_w-200, img_h-60
-            # color = 'Red', width=1.5
-            dc.SetPen(wx.Pen('Red', 1.5, wx.SOLID))
-            dc.DrawLine(x0, y0, x1, y1)
-        if self.circle is not None:
-            x0, y0, rad, color, width = self.circle
-            dc.SetPen(wx.Pen(color, width, wx.SOLID))
-            dc.DrawCircle(x0, y0, rad)
-        dc.EndDrawing()
-
     def Start(self):
         "turn camera on"
-        # print 'Epics AD START ' # self.camera.Connect()
-        # self.cam_name = self.camera.info['modelName']
-        # self.camera.StartCapture()
         self.timer.Start(50)
-        self.autosave_thread.start()
+        if self.autosave_thread is not None:
+            self.autosave_thread.start()
 
     def Stop(self):
         "turn camera off"
         self.timer.Stop()
         self.autosave = False
-        # self.camera.StopCapture()
-        self.autosave_thread.join()
-
-    def onAutosave(self):
-        "autosave process, run in separate thread"
-        # set autosave to False to abort autosaving
-        while self.autosave:
-            tfrac, tint = math.modf(time.time())
-            if tint != self.last_autosave:
-                self.last_autosave = tint
-                try:
-                    self.image.SaveFile(self.autosave_tmpf,
-                                        wx.BITMAP_TYPE_JPEG)
-                    shutil.copy(self.autosave_tmpf,
-                                self.autosave_file)
-                except:
-                    pass
-                tfrac, tint = math.modf(time.time())
-            # sleep for most of the remaining second
-            time.sleep(max(0.05, 0.75*(1.0-tfrac)))
-
-    def SaveImage(self, fname, filetype='jpeg'):
-        """save image (jpeg) to file"""
-        ftype = wx.BITMAP_TYPE_JPEG
-        if filetype.lower() == 'png':
-            ftype = wx.BITMAP_TYPE_PNG
-        elif filetype.lower() in ('tiff', 'tif'):
-            ftype = wx.BITMAP_TYPE_TIFF
-        ## tmpimage = self.GrabWxImage(scale=self.scale, rgb=True)
-        ## tmpimage.SaveFile(fname, ftype)
-        return {'image_size': tmpimage.GetSize(), 
-                'image_format': 'RGB', 
-                'data_format': 'base64',
-                'data': base64.b64encode(tmpimage.GetData())}
-
-    def GrabImage(self):
-        """return base64 encoded image data"""
-        tmpimage = self.camera.GrabWxImage(scale=1, rgb=True)
-        return {'image_size': tmpimage.GetSize(), 
-                'image_format': 'RGB', 
-                'data_format': 'base64',
-                'data': base64.b64encode(tmpimage.GetData())}
+        if self.autosave_thread is not None:
+            self.autosave_thread.join()
 
     def GetImageSize(self):
         self.arrsize = [1,1,1]
@@ -255,14 +133,11 @@ class ImagePanel_EpicsAD(wx.Panel):
 
         rawdata = self.ad_img.PV('ArrayData').get(count=dcount)
         
-        print "==== GrabImage ", im_mode, colormode, width, height, rawdata.dtype
-
         if (colormode == 0 and isinstance(rawdata, np.ndarray) and
             rawdata.dtype != np.uint8):
             im_mode = 'I'
             rawdata = rawdata.astype(np.uint32)
 
-        print "==== GrabImage ", im_mode, width, height, rawdata[:4]
         if im_mode in ('L', 'I'):
             image = wx.EmptyImage(width, height)
             imbuff = Image.frombuffer(im_mode, self.im_size, rawdata,
@@ -274,42 +149,3 @@ class ImagePanel_EpicsAD(wx.Panel):
             image = wx.ImageFromData(width, height, rawdata)
 
         return image.Scale(int(self.scale*width), int(self.scale*height))
-
-
-xx = """
-cname = "%s%s1:"% (self.cam_adpref, self.cam_adform.upper())
-            caput("%sFileName" % cname, fname, wait=True)
-            time.sleep(0.03)
-            caput("%sWriteFile" % cname, 1, wait=True)
-            time.sleep(0.05)
-            img_ok = False
-            t0 = time.time()
-            while not img_ok:
-                if time.time()-t0 > 15:
-                    break
-                try:
-                    out = open(fname, "rb")
-                    imgdata = base64.b64encode(out.read())
-                    out.close()
-                    img_ok = True
-                except:
-                    pass
-                time.sleep(0.05)
-
-    @EpicsFunction
-    def Start(self):
-        if not self.cam_adpref.endswith(':'):
-            self.cam_adpref = "%s:" % self.cam_adpref
-        cname = "%s%s1:"% (self.cam_adpref, self.cam_adform.upper())
-        caput("%sEnableCallbacks" % cname, 1)
-        thisdir = os.path.abspath(os.getcwd())
-        thisdir = thisdir.replace('\\', '/').replace('T:/', '/Volumes/Data/')
-
-        caput("%sFilePath" % cname, thisdir)
-        caput("%sAutoSave" % cname, 0)
-        caput("%sAutoIncrement" % cname, 0)
-        caput("%sFileTemplate" % cname, "%s%s")
-        if self.cam_adform.upper() == 'JPEG':
-            caput("%sJPEGQuality" % cname, 90)
-"""
-
--- sampleviewer/lib/imagepanel_fly2.py
+++ sampleviewer/lib/imagepanel_fly2.py
@@ -4,12 +4,8 @@
 
 import wx
 import time
-import os
-import shutil
-import math
-from threading import Thread
-from cStringIO import StringIO
-import base64
+
+from .imagepanel_base import ImagePanel_Base
 
 HAS_FLY2 = False
 try:
@@ -18,7 +14,7 @@ try:
 except ImportError:
     pass
 
-class ImagePanel_Fly2(wx.Panel):
+class ImagePanel_Fly2(ImagePanel_Base):
     """Image Panel for FlyCapture2 camera"""
     def __init__(self, parent,  camera_id=0, writer=None,
                  autosave_file=None, **kws):
@@ -32,95 +28,16 @@ class ImagePanel_Fly2(wx.Panel):
         self.writer = writer
         self.cam_name = '-'
 
-        
-        self.scale = 0.60
-        self.count = 0
-        self.last_size = 0
-
-        self.scalebar = None
-        self.circle  = None
-        self.SetBackgroundColour("#EEEEEE")
-        self.starttime = time.clock()
-        self.SetBackgroundStyle(wx.BG_STYLE_PAINT)
-
-        self.Bind(wx.EVT_SIZE, self.onSize)
-        self.Bind(wx.EVT_PAINT, self.onPaint)
-        self.Bind(wx.EVT_LEFT_DOWN, self.onLeftDown)
-
-        self.autosave = True
-        self.last_autosave = 0
-        self.autosave_tmpf = None
-        self.autosave_file = None
-        self.autosave_thread = None
-
-        if autosave_file is not None:
-            path, tmp = os.path.split(autosave_file)
-            self.autosave_file = autosave_file
-            self.autosave_tmpf = os.path.join(path, '_tmp_.jpg')
-            self.autosave_thread = Thread(target=self.onAutosave)
-            self.autosave_thread.daemon = True
-
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.onTimer, self.timer)
-
-    def onSize(self, evt):
-        frame_w, frame_h = self.last_size = evt.GetSize()
-        self.scale = min(frame_w/self.img_w, frame_h/self.img_h)
-        self.Refresh()
-        evt.Skip()
-
-    def onTimer(self, evt=None):
-        self.Refresh()
-
-    def onLeftDown(self, evt=None):
-        print 'Left Down Event: ', evt.GetX(), evt.GetY()
-        print 'Left Down Panel Size:  ', self.panel_size
-        print 'Left Down Last Size:   ', self.last_size
-        print 'Left Down Bitmap Size: ', self.bitmap_size
-        print 'Left Down Image Scale: ', self.scale
-
-    def onPaint(self, event):
-        self.count += 1
-        now = time.clock()
-        elapsed = now - self.starttime
-        if elapsed >= 2.0 and self.writer is not None:
-            self.writer(" %.2f fps\n" % (self.count/elapsed))
-            self.starttime = now
-            self.count = 0
-
-        self.scale = max(self.scale, 0.05)
-        self.image = self.camera.GrabWxImage(scale=self.scale, rgb=True)
-        bitmap = wx.BitmapFromImage(self.image)
-
-        img_w, img_h = self.bitmap_size = bitmap.GetSize()
-        pan_w, pan_h = self.panel_size = self.GetSize()
-        pad_w, pad_h = (pan_w-img_w)/2.0, (pan_h-img_h)/2.0
-
-        dc = wx.AutoBufferedPaintDC(self)
-        dc.Clear()
-        dc.DrawBitmap(bitmap, pad_w, pad_h, useMask=True)
-        dc.BeginDrawing()
-        if self.scalebar is not None:
-            x0, x1, y0, y1, color, width = self.scalebar
-            # x0, y0, x1, y1 = img_w-20, img_h-60, img_w-200, img_h-60
-            # color = 'Red', width=1.5
-            dc.SetPen(wx.Pen('Red', 1.5, wx.SOLID))
-            dc.DrawLine(x0, y0, x1, y1)
-        if self.circle is not None:
-            x0, y0, rad, color, width = self.circle
-            dc.SetPen(wx.Pen(color, width, wx.SOLID))
-            dc.DrawCircle(x0, y0, rad)
-        dc.EndDrawing()
 
     def Start(self):
         "turn camera on"
         self.camera.Connect()
-
         self.cam_name = self.camera.info['modelName']
         self.camera.StartCapture()
 
         width, height = self.camera.GetSize()
-        print 'Start Camera, IMAGE SIZE ', self.camera, width, height
         self.img_w = float(width+0.5)
         self.img_h = float(height+0.5)
 
@@ -136,42 +53,6 @@ class ImagePanel_Fly2(wx.Panel):
         if self.autosave_thread is not None:
             self.autosave_thread.join()
 
-    def onAutosave(self):
-        "autosave process, run in separate thread"
-        # set autosave to False to abort autosaving
-        while self.autosave:
-            tfrac, tint = math.modf(time.time())
-            if tint != self.last_autosave:
-                self.last_autosave = tint
-                try:
-                    self.image.SaveFile(self.autosave_tmpf,
-                                        wx.BITMAP_TYPE_JPEG)
-                    shutil.copy(self.autosave_tmpf,
-                                self.autosave_file)
-                except:
-                    pass
-                tfrac, tint = math.modf(time.time())
-            # sleep for most of the remaining second
-            time.sleep(max(0.05, 0.75*(1.0-tfrac)))
+    def GrabWxImage(self, scale=1, rgb=True):
+        return self.camera.GrabWxImage(scale=scale, rgb=rgb)
 
-    def SaveImage(self, fname, filetype='jpeg'):
-        """save image (jpeg) to file"""
-        ftype = wx.BITMAP_TYPE_JPEG
-        if filetype.lower() == 'png':
-            ftype = wx.BITMAP_TYPE_PNG
-        elif filetype.lower() in ('tiff', 'tif'):
-            ftype = wx.BITMAP_TYPE_TIFF
-        tmpimage = self.camera.GrabWxImage(scale=1, rgb=True)
-        tmpimage.SaveFile(fname, ftype)
-        return {'image_size': tmpimage.GetSize(), 
-                'image_format': 'RGB', 
-                'data_format': 'base64',
-                'data': base64.b64encode(tmpimage.GetData())}
-
-    def GrabImage(self):
-        """return base64 encoded image data"""
-        tmpimage = self.camera.GrabWxImage(scale=1, rgb=True)
-        return {'image_size': tmpimage.GetSize(), 
-                'image_format': 'RGB', 
-                'data_format': 'base64',
-                'data': base64.b64encode(tmpimage.GetData())}
