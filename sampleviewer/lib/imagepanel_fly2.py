1a4f2f0133daa865917c7e3f9e01a4033321c2c2
Matt Newville <newville@cars.uchicago.edu>
2017-11-01 17:39:43 -0500

    using datapush, and not autosave_file

--- sampleviewer/lib/imagepanel_fly2.py
+++ sampleviewer/lib/imagepanel_fly2.py
@@ -36,7 +36,8 @@ class ImagePanel_Fly2(ImagePanel_Base):
         super(ImagePanel_Fly2, self).__init__(parent, -1,
                                               size=(800, 600),
                                               writer=writer,
-                                              autosave_file=autosave_file, **kws)
+                                              autosave_file=autosave_file,
+                                              datapush=False, **kws)
         self.camera = fly2_camera.Fly2Camera(camera_id=camera_id)
         self.output_pv = output_pv
         self.output_pvs = {}
@@ -72,16 +73,11 @@ class ImagePanel_Fly2(ImagePanel_Base):
             self.output_pvs['ArraySize0_RBV'].put(int(self.img_w))
 
         self.timer.Start(50)
-        if self.autosave_thread is not None:
-            self.autosave_thread.start()
 
     def Stop(self):
         "turn camera off"
         self.timer.Stop()
-        self.autosave = False
         self.camera.StopCapture()
-        if self.autosave_thread is not None:
-            self.autosave_thread.join()
 
     def SetExposureTime(self, exptime):
         self.camera.SetPropertyValue('shutter', exptime, auto=False)
@@ -138,8 +134,8 @@ class ImagePanel_Fly2(ImagePanel_Base):
             img = img.convert(PyCapture2.PIXEL_FORMAT.RGB)
         self.data_shape = (nrows, ncols, 3)
         self.data = np.array(img.getData())
-        return wx.Image(ncols, nrows, self.data).Rescale(width, height,
-                                                        quality=quality)
+        self.full_image = wx.Image(ncols, nrows, self.data)
+        return self.full_image.Rescale(width, height, quality=quality)
 
     def GrabNumpyImage(self):
         return self.camera.GrabNumPyImage(format='rgb')
@@ -210,6 +206,13 @@ class ConfPanel_Fly2(ConfPanel_Base):
                 sizer.Add(wids[akey], (i, 2), (1, 1), LEFT)
             i += 1
 
+        #
+        wids['datapush'] =  wx.CheckBox(self, -1, label='Show Image on Hutch Computer')
+        wids['datapush'].SetValue(0)
+        wids['datapush'].Bind(wx.EVT_CHECKBOX, self.onEnableDataPush)
+
+        sizer.Add(wids['datapush'], (i+1, 0), (1, 3), LEFT)
+
         pack(self, sizer)
         self.__initializing = False
         self.timer = wx.Timer(self)
@@ -231,11 +234,8 @@ class ConfPanel_Fly2(ConfPanel_Base):
         cinfo  = self.image_panel.camera.info
         self.title.SetLabel("Camera Model: %s" % (cinfo.modelName))
         self.title2.SetLabel("Serial #%d, Firmware %s" % (cinfo.serialNumber, cinfo.firmwareVersion))
-        # print( "Camera info: ")
-        # for a in dir(cinfo):
-        # if a.startswith('__'):
-        #         continue
-        #     print(a, getattr(cinfo, a, '??'))
+    def onEnableDataPush(self, evt=None, **kws):
+        self.image_panel.datapush = evt.IsChecked()
 
     def onTimer(self, evt=None, **kws):
         for prop in ('shutter', 'gain', 'gamma', 'white_balance'):
