72f30402068315259e493b605ebec8a0c708d51e
Matthew Newville <newville@cars.uchicago.edu>
2019-01-11 15:20:54 -0600

    update setup.py

--- AreaDetector_Display/setup.py
+++ AreaDetector_Display/setup.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, setup_keywords
+from setuptools import setup
 
-deps = ('wx', 'epics', 'numpy', 'Image')
-setup(name = 'epicsapp_ad_display',
+deps = ('wx', 'epics', 'numpy')
+setup(name = 'epicsad_display',
       version = '0.2',
       author = 'Matthew Newville',
       author_email = 'newville@cars.uchicago.edu',
       license = 'BSD',
       description = 'Epics AreaDetector Viewer',
-      package_dir = {'epicsapps.ad_display': 'lib', 'epicsapps': 'base'},
-      packages = ['epicsapps', 'epicsapps.ad_display'],
-      data_files  = [('bin', ['pyepics_ad_display.py'])])
-
-
-errmsg = 'WARNING: pyepics_ad_display requires Python module "%s"'
-
-for mod in deps:
-    try:
-        a = __import__(mod)
-    except ImportError:
-        print(errmsg % mod)
+      packages = ['epicsad_display', 'epicsad_display.icons'],
+      package_data={'epicsad_display': ['icons/*']},
+      )
