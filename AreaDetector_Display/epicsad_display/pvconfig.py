54b951ec490345d7ca37b8cd9dc86073b7b8ff57
Matt Newville <newville@cars.uchicago.edu>
2019-01-22 14:41:35 -0600

    pv config add support for non-prefixed PVs, font size

--- AreaDetector_Display/epicsad_display/pvconfig.py
+++ AreaDetector_Display/epicsad_display/pvconfig.py
@@ -1,9 +1,10 @@
 import wx
-from epics import get_pv
+
+from epics import get_pv, poll
 from epics.wx.utils import pack
 from epics.wx import (PVEnumChoice, PVEnumButtons,
                       PVFloatCtrl, PVTextCtrl, PVStaticText)
-
+from wxutils import Font
 labstyle = wx.ALIGN_LEFT|wx.EXPAND|wx.ALIGN_CENTER_VERTICAL
 
 
@@ -29,14 +30,25 @@ class PVConfigPanel(wx.Panel):
         self.wids = {}
         self.prefix = prefix
         irow = -1
-        for label, pvsuff, dtype, rsuff, wsize in pvdata:
+        for label, pvname, use_prefix, dtype, rsuff, wsize, fontsize in pvdata:
+            if use_prefix:
+                pvname = prefix + pvname
+            get_pv(pvname)
+        poll(0.02)
 
-            pvname = prefix + pvsuff
+        for label, pvname, use_prefix, dtype, rsuff, wsize, fontsize in pvdata:
+            label = '  %s' % label
+            if use_prefix:
+                pvname = prefix + pvname
             ctrl  = pv_control(dtype)
-            self.wids[label] = ctrl(self, pv=get_pv(pvname), size=(wsize, -1))
+            opts = dict(pv=get_pv(pvname), size=(wsize, -1))
+
+            self.wids[label] = ctrl(self, **opts)
+            self.wids[label].SetFont(Font(fontsize))
 
             title = wx.StaticText(self, label=label, size=(8*len(label), -1),
                                   style=labstyle)
+            title.SetFont(Font(fontsize))
             irow += 1
             sizer.Add(title, (irow, 0), (1, 1), labstyle)
 
@@ -45,7 +57,7 @@ class PVConfigPanel(wx.Panel):
                 rname = pvname + rsuff
                 self.wids[rlabel] = PVStaticText(self, pv=get_pv(rname),
                                                  size=(wsize, -1))
-
+                self.wids[rlabel].SetFont(Font(fontsize))
                 sizer.Add(self.wids[label], (irow, 1), (1, 1), labstyle)
                 sizer.Add(self.wids[rlabel], (irow, 2), (1, 1), labstyle)
             else:
