c4ebec1b1e34fd95b6e5d176c6599c23dc5611fa
Matt Newville <newville@cars.uchicago.edu>
2019-09-27 15:09:08 -0500

    update requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup for pyepics/epicsapps
 from setuptools import setup, find_packages
 
 install_requires = ('pyepics', 'numpy', 'matplotlib', 'xraydb', 'wxpython',
-                    'wxmplot', 'sqliachemy', 'pyyaml')
+                    'wxmplot', 'sqlalchemy', 'pyyaml')
 
 packages = ['epicsapps']
 for pname in find_packages('epicsapps'):
