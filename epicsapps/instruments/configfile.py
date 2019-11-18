9aaf53aa472280c4b43741cef6ebd46ef31b9b8c
Matthew Newville <newville@cars.uchicago.edu>
2019-11-18 11:08:04 -0600

    allow for configfile of None on initial startup

--- epicsapps/instruments/configfile.py
+++ epicsapps/instruments/configfile.py
@@ -1,6 +1,5 @@
-
-from ..utils import ConfigFile, get_default_configfile
-
+import os
+from ..utils import ConfigFile, get_default_configfile, get_configfolder
 CONFFILE = 'instruments.yaml'
 
 class InstrumentConfig(ConfigFile):
@@ -9,4 +8,7 @@ class InstrumentConfig(ConfigFile):
             default_config = dict(server='sqlite', dbname=None, host=None,
                                   port='5432', user=None, password=None,
                                   recent_dbs=[])
+        if fname is None:
+            fname = os.path.join(get_configfolder(), CONFFILE)
+       
         ConfigFile.__init__(self, fname, default_config=default_config)
