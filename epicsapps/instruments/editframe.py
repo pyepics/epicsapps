16489612d11c7a36e5bb7d7c17c2512cf0554124
Matt Newville <newville@cars.uchicago.edu>
2023-09-08 13:22:39 -0500

    fixes for editing positions

--- epicsapps/instruments/editframe.py
+++ epicsapps/instruments/editframe.py
@@ -11,7 +11,7 @@ from epics.wx.utils import add_menu
 from wxutils import (NumericCombo, pack, SimpleText, FileSave, FileOpen,
                      SelectWorkdir, Button)
 
-from .utils import GUIColors, YesNo, set_font_with_children, get_pvtypes
+from .utils import GUIColors, YesNo, set_font_with_children
 from . import instrument
 
 class PVTypeChoice(wx.Choice):
@@ -43,7 +43,7 @@ class pvNameCtrl(wx.TextCtrl):
         key   = event.GetKeyCode()
         entry = wx.TextCtrl.GetValue(self).strip()
         pos   = wx.TextCtrl.GetSelection(self)
-        if (key == wx.WXK_RETURN):
+        if key == wx.WXK_RETURN:
             self.owner.connect_pv(entry, wid=self.GetId())
         event.Skip()
 
@@ -90,8 +90,8 @@ class NewPositionFrame(wx.Frame, FocusEventFrame) :
         self.parent = parent
         self.page = page
         self.db = db
+        self.instname = instname
         self.inst = db.get_instrument(instname)
-        print("new position " , instname, self.inst)
         STY  = wx.GROW|wx.ALL
         LSTY = wx.ALIGN_LEFT|wx.GROW|wx.ALL
         RSTY = wx.ALIGN_RIGHT|STY
@@ -99,10 +99,7 @@ class NewPositionFrame(wx.Frame, FocusEventFrame) :
         CEN  = wx.ALIGN_CENTER|wx.GROW|wx.ALL
         LEFT = wx.ALIGN_LEFT|wx.GROW|wx.ALL
 
-
         self.name =  wx.TextCtrl(panel, value='', size=(200, -1))
-
-
         sizer = wx.GridBagSizer(12, 3)
 
         ir = 0
@@ -121,13 +118,13 @@ class NewPositionFrame(wx.Frame, FocusEventFrame) :
         sizer.Add(wx.StaticLine(panel, size=(195, -1), style=wx.LI_HORIZONTAL),
                   (ir, 0), (1, 3), CEN, 2)
 
-        self.positions = []
+        self.positions = {}
         ir += 1
         for pvname in self.db.get_instrument_pvs(instname):
             val =  wx.TextCtrl(panel, value='', size=(150, -1))
             sizer.Add(SimpleText(panel, pvname), (ir, 0), (1, 1), LSTY, 2)
             sizer.Add(val,                       (ir, 1), (1, 1), LSTY, 2)
-            self.positions.append(val)
+            self.positions[pvname] = val
             ir += 1
 
         sizer.Add(wx.StaticLine(panel, size=(195, -1), style=wx.LI_HORIZONTAL),
@@ -160,12 +157,12 @@ class NewPositionFrame(wx.Frame, FocusEventFrame) :
         posname = self.name.GetValue()
         values = {}
         valid = len(posname) > 0
-        for pv, newval in zip(self.inst.pvs, self.positions):
-            val = str(newval.GetValue())
-            values[pv.name] = val
-            valid = valid and len(val) > 0
+        for pvname, tc_wid in self.positions.items():
+            val = str(tc_wid.GetValue()).strip()
+            if len(val) > 0:
+                values[pvname] = val
         if valid:
-            self.db.save_position(posname, self.inst, values)
+            self.db.save_position(posname, self.instname, values)
             if self.page is not None:
                 poslist = self.page.pos_list
                 if posname not in poslist.GetItems():
@@ -180,10 +177,11 @@ class EditInstrumentFrame(wx.Frame, FocusEventFrame) :
     def __init__(self, parent=None, pos=(-1, -1),
                  instname=None, db=None):
 
+        self.parent = parent
+        self.db = db
         self.epics_pvs = {}
-
         title = 'Add New Instrument'
-        if inst is not None:
+        if instname is not None:
             title = f'Edit Instrument  {instname}'
 
         style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL
@@ -204,8 +202,6 @@ class EditInstrumentFrame(wx.Frame, FocusEventFrame) :
         titlefont.PointSize += 1
         titlefont.SetWeight(wx.BOLD)
 
-        self.parent = parent
-        self.db = db
         self.inst = db.get_instrument(instname)
         self.connecting_pvs = {}
 
@@ -247,40 +243,22 @@ class EditInstrumentFrame(wx.Frame, FocusEventFrame) :
                       (2, 2), (1, 1), RSTY, 2)
 
             instpvs =  db.get_instrument_pvs(instname)
-            # move_choices = ["1"]
-            # if len(ordered_pvs) > 1:
-            #   move_choices = ["%i" % (i+1) for i in range(len(ordered_pvs))]
 
-            for instpv in instpvs:
-                print(instpv)
-                pv = instpv.pv
-                # move_order = instpv.move_order
-                # if move_order is None:
-                #     move_order = 1
-                # move_order = int(move_order)
+            for pvname, dat in instpvs.items():
+                pv_id, pv = dat
                 irow += 1
-                pvchoices = get_pvtypes(pv, instrument)
+                pvchoices = self.db.get_pvtypes(pv)
 
-                label= SimpleText(panel, pv.name,  minsize=(175, -1),
+                label= SimpleText(panel, pvname,  minsize=(175, -1),
                                   style=LSTY)
-
-                try:
-                    itype = pvchoices.index(pv.pvtype.name)
-                except ValueError:
-                    itype = 0
-
                 pvtype = PVTypeChoice(panel, choices=pvchoices)
-                pvtype.SetSelection(itype)
-                pvtype.SetStringSelection(pv.pvtype.name)
+                pvtype.SetSelection(0)
                 del_pv = YesNo(panel, defaultyes=False, size=(60, -1))
-                # tc_order =  wx.Choice(panel, choices=move_choices, size=(50, -1))
-                # tc_order.SetSelection(min(len(move_choices)-1, max(0, move_order - 1)))
 
-                self.curpvs.append((instpv, label, pvtype, del_pv))
+                self.curpvs.append((pvname, label, pvtype, del_pv))
 
                 sizer.Add(label,    (irow, 0), (1, 1), LSTY,  3)
                 sizer.Add(pvtype,   (irow, 1), (1, 1), CSTY,  3)
-                # sizer.Add(tc_order, (irow, 2), (1, 1), RSTY,  3)
                 sizer.Add(del_pv,   (irow, 2), (1, 1), RSTY,  3)
 
             irow += 1
@@ -300,33 +278,23 @@ class EditInstrumentFrame(wx.Frame, FocusEventFrame) :
                              colour=self.colors.title, style=CSTY),
                   (irow, 2), (1, 1), RSTY, 2)
 
-        # sizer.Add(SimpleText(panel, 'Move Order:',
-        #                     colour=self.colors.title, style=CSTY),
-        #          (irow, 3), (1, 1), LSTY, 2)
-        # New PVs
         for npv in range(8):
             irow += 1
             name = pvNameCtrl(self, panel, value='', size=(175, -1))
             pvtype = PVTypeChoice(panel)
             del_pv = YesNo(panel, defaultyes=False, size=(60, -1))
-            # tc_order =  wx.Choice(panel, choices=move_choices, size=(50, -1))
-            # tc_order.SetSelection(0)
             pvtype.Disable()
-            # tc_order.Disable()
             del_pv.Disable()
             sizer.Add(name,     (irow, 0), (1, 1), LSTY,  3)
             sizer.Add(pvtype,   (irow, 1), (1, 1), CSTY,  3)
-            # sizer.Add(tc_order, (irow, 2), (1, 1), CSTY,  3)
             sizer.Add(del_pv,   (irow, 2), (1, 1), RSTY,  3)
-
             self.newpvs[name.GetId()] = dict(index=npv, name=name,
-                                             # tc_order=tc_order,
                                              type=pvtype, delpv=del_pv)
 
         btn_panel = wx.Panel(panel, size=(75, -1))
         btn_sizer = wx.BoxSizer(wx.HORIZONTAL)
         btn_ok     = Button(btn_panel, 'Done',     size=(70, -1),
-                                action=self.OnDone)
+                                action=self.onDone)
         btn_cancel = Button(btn_panel, 'Cancel', size=(70, -1), action=self.onCancel)
 
         btn_sizer.Add(btn_ok,     0, wx.ALL,  2)
@@ -352,6 +320,8 @@ class EditInstrumentFrame(wx.Frame, FocusEventFrame) :
         self.Show()
         self.Raise()
 
+
+
     def get_page_map(self):
         out = {}
         for i in range(self.parent.nb.GetPageCount()):
@@ -361,137 +331,116 @@ class EditInstrumentFrame(wx.Frame, FocusEventFrame) :
     @EpicsFunction
     def connect_pv(self, pvname, wid=None):
         """try to connect newly added epics PVs"""
-        if pvname is None or len(pvname) < 1:
+        if pvname in (None, '', 'None') or len(pvname) < 1:
             return
         if pvname not in self.connecting_pvs:
             if pvname not in self.epics_pvs:
-                self.epics_pvs[pvname] = epics.PV(pvname)
-            self.connecting_pvs[pvname] = (wid, time.time())
+                self.epics_pvs[pvname] = epics.get_pv(pvname)
+            self.connecting_pvs[pvname] = (wid, time.time()+15.0)
             if not self.etimer.IsRunning():
-                self.etimer.Start(150)
+                self.etimer.Start(100)
 
     def onTimer(self, event=None):
-        "timer event handler: look for connecting_pvs give up after 30 seconds"
+        "timer event handler: look for connecting_pvs give up after 15 seconds"
         if len(self.connecting_pvs) == 0:
             self.etimer.Stop()
-        try:
-            for pvname in self.connecting_pvs:
-                self.new_pv_connected(pvname)
-                if time.time() - self.connecting_pvs[pvname][1] > 15:
-                    self.connecting_pvs.pop(pvname)
-        except:
-            pass
-
+        cnames = list(self.connecting_pvs.keys())
+        for pvname in cnames:
+            wid, expire_time = self.connecting_pvs[pvname]
+            if self.epics_pvs[pvname].connected:
+                self.new_pv_connected(pvname, wid)
+            if self.epics_pvs[pvname].connected or time.time() > expire_time:
+                self.connecting_pvs.pop(pvname)
 
     @EpicsFunction
-    def new_pv_connected(self, pvname):
+    def new_pv_connected(self, pvname, wid):
         """if a new epics PV has connected, fill in the form data"""
-        if pvname not in self.epics_pvs:
-            pv = self.epics_pvs[pvname] = epics.PV(pvname)
-        else:
-            pv = self.epics_pvs[pvname]
-            pv.poll()
-
-        if not pv.connected:
+        pv = self.epics_pvs.get(pvname, None)
+        if pv is None or not pv.connected:
             return
-        try:
-            wid, itime = self.connecting_pvs.pop(pvname)
-        except KeyError:
-            wid = None
-        pv.get_ctrlvars()
 
+        pv.get_ctrlvars()
         self.newpvs[wid]['type'].Enable()
-        # self.newpvs[wid]['tc_order'].Enable()
         self.newpvs[wid]['delpv'].Enable()
 
-        pvchoices = get_pvtypes(pv, instrument)
+        pvchoices = self.db.get_pvtypes(pv)
         self.newpvs[wid]['type'].SetChoices(pvchoices)
         self.newpvs[wid]['type'].SetSelection(0)
-        # self.newpvs[wid]['tc_order'].SetSelection(0)
         self.newpvs[wid]['delpv'].SetStringSelection('No')
 
-    def OnDone(self, event=None):
+    def onDone(self, event=None):
         """ Done Button Event: save and exit"""
-        # print("EditInstrumentFrame OnDone ", event)
         instpanel = self.parent.nb.GetCurrentPage()
         db = instpanel.db
-        inst = instpanel.inst
-        instpvs = db.get_ordered_instpvs(inst)
+        instname = instpanel.instname
+
+        instpvs = db.get_instrument_pvs(instname)
         pagemap = self.get_page_map()
-        page = pagemap.get(inst.name, None)
+        page = pagemap.get(instname, None)
 
         newname = self.name.GetValue()
-        oldname = inst.name
-        if newname != oldname:
-            inst.name = newname
-
-        if page is not None:
-            self.parent.nb.SetPageText(page, newname)
-            instpanel.inst_title.SetLabel(' %s ' % newname)
+        if newname != instname:
+            db.update('instrument', where={'name': instname}, name=newname)
+            instname = newname
+            instpanel.inst_title.SetLabel(f' {newname} ')
+            instpanel.instname = newname
+            if page is not None:
+                self.parent.nb.SetPageText(page, newname)
 
         # make sure newpvs from dictionary are inserted in order
         # of "index", as appear on the GUI screen.
-        new_pvs = [None]* len(self.newpvs)
+        new_pvnames = []
+        new_pvtypes = []
         for entry in self.newpvs.values():
             if entry['delpv'].GetSelection() == 0 and entry['type'].Enabled:
                 pvname = str(entry['name'].GetValue().strip())
                 if len(pvname) > 0:
                     pvtype = str(entry['type'].GetStringSelection())
-                    # tc_order = int(entry['tc_order'].GetSelection() + 1)
-                    new_pvs[entry['index']] = (pvname, pvtype)
-
+                    new_pvnames.append(pvname)
+                    new_pvtypes.append(pvtype)
 
         self.etimer.Stop()
         for i in list(self.connecting_pvs.keys()):
             self.connecting_pvs.pop(i)
 
-        for newpvs in new_pvs:
-            if newpvs is not None:
-                pvname, pvtype = newpvs
+        if len(new_pvnames) > 0:
+            for pvname, pvtype in zip(new_pvnames, new_pvtypes):
                 db.add_pv(pvname, pvtype=pvtype)
-                inst.pvs.append(db.get_pv(pvname))
+            db.add_instrument_pvs(instname, new_pvnames)
+            for pvname in new_pvnames:
                 instpanel.add_pv(pvname)
-                # print("Added pvname ", pvname, instpanel)
-                # for instpv in db.get_ordered_instpvs(inst):
-                #     if pvname == instpv.pv.name:
-                        # instpv.move_order = tc_order
 
         for ipv, lctrl, typectrl, delctrl in  self.curpvs:
             # ipv.move_order = int(tc_order.GetSelection() + 1)
-            pvname = ipv.pv.name
+            pvname = ipv
             if delctrl.GetSelection() == 1:
-                thispv = db.get_pv(pvname)
-                inst.pvs.remove(thispv)
+                db.remove_instrument_pv(instname, pvname)
                 instpanel.undisplay_pv(pvname)
             else:
                 newtype = typectrl.GetStringSelection()
-                curtype= db.get_pv(pvname).pvtype.name
+                curtype= db.get_pvtype(pvname)
                 if newtype != curtype:
                     db.set_pvtype(pvname, newtype)
                     instpanel.PV_Panel(pvname)
 
-        db.commit()
-
         pagemap = self.get_page_map()
         self.parent.nb.DeletePage(pagemap[newname])
-        self.parent.add_instrument_page(inst)
-        time.sleep(0.5)
+        self.parent.add_instrument_page(instname)
+        time.sleep(0.25)
         self.etimer.Stop()
 
         # set order for PVs (as for next time)
-        inst = db.get_instrument(inst.name)
-        instpvs = db.get_ordered_instpvs(inst)
-        for opv in instpvs:
-            opv.display_order = -1
-        for i, pv in enumerate(inst.pvs):
-            for opv in instpvs:
-                if opv.pv == pv:
-                    opv.display_order = i
-
-        for opv in instpvs:
-            opv.display_order = i
-            i = i + 1
-        db.commit()
+        inst = db.get_instrument(instname)
+        instpvs = db.get_instrument_pvs(instname)
+        # for opv in instpvs:
+        #     opv.display_order = -1
+        # for i, pv in enumerate(inst.pvs):
+        #    for opv in instpvs:
+        #        if opv.pv == pv:
+        #            opv.display_order = i
+        # for opv in instpvs:
+        #    opv.display_order = i
+        #    i = i + 1
 
         self.Destroy()
 
@@ -525,7 +474,6 @@ class ErasePositionsFrame(wx.Frame, FocusEventFrame) :
 
 
         inst = db.get_instrument(page.instname)
-        print(page.instname, inst, db.get_positions(page.instname))
         posnames = [p.name for p in db.get_positions(page.instname)]
 
         ALL_EXP  = wx.ALL|wx.EXPAND
