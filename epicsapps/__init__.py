f545fa30597f655c3819e214db531915f2c076af
Matthew Newville <newville@cars.uchicago.edu>
2025-01-24 14:33:06 -0600

    updates for distribution

--- epicsapps/__init__.py
+++ epicsapps/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from .apps import (run_epicsapps, run_adviewer, run_instruments,
                    run_stripchart, run_pvlogger)
-
 from .version import __version__, __version_tuple__
+
+import areadetector, utils, instruments, pvlogger, stripcahrt, microscope, microscope
