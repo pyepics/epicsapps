4cfc2cc6d266eb53e79bba2678f1b80053ce460c
Matthew Newville <newville@cars.uchicago.edu>
2025-01-24 13:29:01 -0600

    update sqlite2pg example

--- examples/instruments/sqlite2pg.py
+++ examples/instruments/sqlite2pg.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 """
-
 This will convert an Epics Instruments file in SQLite
 to one using PostgresQL ScanDB
+
+requires epicsscan to be installed
 """
 
 from epicsscan import ScanDB, InstrumentDB
@@ -16,23 +17,25 @@ username = 'db_user'
 password = 'something_super_secret'
 host     = 'cars4.cars.aps.anl.gov'
 
-pdb = ScanDB(dbname, server='postgresql', host=host,
-               user=username, password=password)
+def sqlite2pg(fname, dbname, username, password, host):
+    sdb = ScanDB(fname, server='sqlite3')
+    pdb = ScanDB(dbname, server='postgresql', host=host,
+                 user=username, password=password)
 
-sdb = ScanDB(fname, server='sqlite3')
+    s_idb = InstrumentDB(sdb)
+    p_idb = InstrumentDB(pdb)
 
-p_idb = InstrumentDB(pdb)
-s_idb = InstrumentDB(sdb)
+    for row in s_idb.get_all_instruments():
+        instname = row.name
+        print("Get Sqlite Instrument ", instname)
+        inst = s_idb.get_instrument(instname)
+        for posname in s_idb.get_positionlist(instname):
+            pvals = s_idb.get_position_vals(instname, posname)
 
-for row in s_idb.get_all_instruments():
-    instname = row.name
-    print("Get Sqlite Instrument ", instname)
-    inst = s_idb.get_instrument(instname)
-    for posname in s_idb.get_positionlist(instname):
-        pvals = s_idb.get_position_vals(instname, posname)
+            if p_idb.get_instrument(instname) is None:
+                print("Add PG Instrument ", instname)
+                p_idb.add_instrument(instname, pvs=list(pvals.keys()))
+            print("Save position ", instname, posname, pvals)
+            p_idb.save_position(instname, posname, pvals)
 
-        if p_idb.get_instrument(instname) is None:
-            print("Add PG Instrument ", instname)
-            p_idb.add_instrument(instname, pvs=list(pvals.keys()))
-        print("Save position ", instname, posname, pvals)
-        p_idb.save_position(instname, posname, pvals)
+    print("done")
