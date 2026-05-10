e518616d7a1256a61487fd949f20ddac3d4cba4c
Matthew Newville <newville@cars.uchicago.edu>
2026-05-10 14:31:58 -0500

    export check_pvlog_timestamp

--- epicsapps/pvlogger/__init__.py
+++ epicsapps/pvlogger/__init__.py
@@ -1,8 +1,9 @@
-from .pvlogger import PVLogger
+from .pvlogger import PVLogger, check_pvlog_timestamp
 from .logfile import read_logfile, read_logfolder
 from ..utils import HAS_WXPYTHON
 PVLoggerApp = None
 if HAS_WXPYTHON:
     from .pvlogger_app import PVLoggerApp
 
-__all__ = ('PVLogger', 'PVLoggerApp', 'read_logfile', 'read_logfolder')
+__all__ = ('PVLogger', 'PVLoggerApp', 'check_pvlog_timestamp',
+           'read_logfile', 'read_logfolder')
