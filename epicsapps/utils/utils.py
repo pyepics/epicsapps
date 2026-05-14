ca5b51329ebc83857e818240d72437cd920cce16
Matthew Newville <newville@cars.uchicago.edu>
2026-05-14 12:21:00 -0500

    fix utilities

--- epicsapps/utils/utils.py
+++ epicsapps/utils/utils.py
@@ -16,6 +16,8 @@ def normalize_pvname(pvname):
 
 def get_pvdesc(pvname):
     "get description value for a PV"
+    desc = pref = pvname
+    descpv = epics.get_pv(pref + '.DESC', form='native')
     desc = pvname
     pref = get_pvroot(pvname)
     descpv = epics.get_pv(f'{pref}.DESC', form='native')
@@ -25,43 +27,9 @@ def get_pvdesc(pvname):
 
 def get_pvmdel(pvname):
     "get MDEL value for a PV"
-    desc = pvname
+    mdel = 0
     pref = get_pvroot(pvname)
-    descpv = epics.get_pv(pref + '.MDEL', form='native')
-    if descpv.connect():
-        desc = descpv.get()
-    return desc
- import os
- import epics
-
-def get_pvroot(pvname):
-    """strip prvoot from PV name"""
-    out = str(pvname)
-    if '.' in pvname:
-        out = pvname[:pvname.find('.')]
-    return out
-
- def normalize_pvname(pvname):
-     """return pvname, with ending .VAL if no suffix was given"""
-     if '.' not in pvname:
-         pvname = f'{pvname}.VAL'
-     return pvname
-
- def get_pvdesc(pvname):
-     "get description value for a PV"
-     desc = pref = pvname
-     descpv = epics.get_pv(pref + '.DESC', form='native')
-     desc = pvname
-     pref = get_pvroot(pvname)
-     descpv = epics.get_pv(f'{pref}.DESC', form='native')
-     if descpv.connect():
-         desc = descpv.get()
-     return desc
-
-def get_pvmdel(pvname):
-    "get MDEL value for a PV"
-    desc = pvname
-    pref = get_pvroot(pvname)
-    descpv = epics.get_pv(pref + '.MDEL', form='native')
-    if descpv.connect():
-        desc = descpv.get()
+    mdelpv = epics.get_pv(pref + '.MDEL', form='native')
+    if mdelpv.connect():
+        mdel = mdelpv.get()
+    return mdel
