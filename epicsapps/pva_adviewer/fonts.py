4d15c863eff202b27ee3827079e0c4aefaa4d12e
Christofanis Skordas <skordasc@uchicago.edu>
2026-07-14 09:56:22 -0500

    adds fonts

---
+++ epicsapps/pva_adviewer/fonts.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+Provides a scaled font utility for wxPython controls.
+"""
+
+import sys
+import wx
+
+__all__ = ["scaled_font"]
+
+_PT_TO_PX = {9: 10, 10: 11, 11: 12, 12: 13, 13: 14}
+_WIN_PX_ADJUST = -2
+
+
+def scaled_font(
+    pt: int,
+    family: int = wx.FONTFAMILY_DEFAULT,
+    style: int = wx.FONTSTYLE_NORMAL,
+    weight: int = wx.FONTWEIGHT_NORMAL,
+) -> wx.Font:
+    px = _PT_TO_PX.get(pt, pt)
+    if sys.platform == "win32":
+        px = max(1, px + _WIN_PX_ADJUST)
+    return wx.Font(wx.Size(0, px), family, style, weight)
